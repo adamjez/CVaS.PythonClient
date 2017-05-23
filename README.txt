a6b339cee6705dad7a057b982fb86cff271a63f7
Adam Jez <adamjez@outlook.cz>
2017-05-24 01:32:16 +0200

    Added readme

---
+++ README.txt
@@ -0,0 +1,5 @@
+Author: Adam Jez <xjezad00@stud.fit.vutbr.cz>
+Source: https://github.com/adamjez/CVaS.PythonClient
+
+Install instruction:
+python setup.py install
--- Test/retrievingRun.py
+++ Test/retrievingRun.py
@@ -1,6 +1,6 @@
 import cvas
 
-client = cvas.client("zVes/1008G1YhtKNiNa0WY5ZGubLK+DYYu0g+e1hcmQ=", "http://localhost:5000")
+client = cvas.client("fpfGmGL99O+3bM1BpV8vSQLeHxxocf+IeLMcKHFwfXU=", "http://localhost:5000")
 
-run = client.run("fd15273c-6d9a-4dba-558c-08d472df7446")
+run = client.run("0ca1edcf-a535-4c5f-7048-08d4a22cf837")
 print(run.std_out)
